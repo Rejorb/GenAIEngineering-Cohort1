37cd5f518d203d1b65daa5082f3a723471421c7e
nickdelgiudice <nicola.del.giudice@gmail.com>
2025-04-12 20:56:43 +0200

    My edits to Day_1 notebooks + additional files

---
+++ Day_1/WhileUserInput.py
@@ -0,0 +1,9 @@
+Input = 'x'
+
+# Be in loop, seek input from user. As long as not 'exit', continue in loop
+while (Input != 'exit') and (Input != 'Exit'):
+
+    Input = input ("Say something or 'exit' ...")
+
+    if (Input != 'exit') and (input != 'Exit'):
+        print (Input)
